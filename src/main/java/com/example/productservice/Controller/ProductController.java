5072e5dd025e127dd101816c2e414e4d65cbea3d
Akhil B Menon <59950744+abm2707@users.noreply.github.com>
2025-05-23 23:25:12 +0530

    Update ProductController.java

--- src/main/java/com/example/productservice/Controller/ProductController.java
+++ src/main/java/com/example/productservice/Controller/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+// Test comment to check Git Webhook
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
